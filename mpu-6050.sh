3e8683614f2d298be0dbce9320b02028456f72f2
arduino90120 <60314719+arduino90120@users.noreply.github.com>
2022-03-06 11:27:44 +0100

    Add files via upload

---
+++ mpu-6050.sh
@@ -0,0 +1,11 @@
+sudo apt update 
+
+sudo apt install -y python3-smbus
+
+git clone https://github.com/Arijit1080/mpu6050-with-Raspberry-Pi
+
+pip3 install mpu6050-raspberrypi
+
+cd mpu6050-with-RaspberryPi
+
+python3 mpu_6050.py
